262c77ef5446dc6007676480ec61a31570a6006b
01pooja10 <66198904+01pooja10@users.noreply.github.com>
2021-05-21 20:03:43 +0530

    Added code for creating dataset

--- code/dataset.py
+++ code/dataset.py
@@ -1,1 +1,37 @@
-from torch.utils.data import DataLoader
+import os
+import PIL
+from PIL import Image
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+class ShoeData(Dataset):
+    def __init__(self,root_directory):
+        super(ShoeData,self).__init__()
+        self.root = root_directory
+        self.files = os.listdir(root_directory)
+        print(files)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self,idx):
+        imfile = self.files[idx]
+        impath = os.path.join(self.root,impath)
+        img = Image.open(impath)
+        img = np.array(img)
+        inp = img[:,:256,:]
+        out = img[:,256:,:]
+        return
+
+'''
+img = np.array(Image.open(r'C:\Users\Pooja\Documents\ML_projects\Sketch-to-Shoe\data\28_AB.jpg'))
+ipimg = img[:,:256,:]
+opimg = img[:,256:,:]
+
+ipimg = Image.fromarray(ipimg)
+ipimg.show()
+
+opimg = Image.fromarray(opimg)
+opimg.show()
+'''
